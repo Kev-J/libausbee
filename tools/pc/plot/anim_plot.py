813cc97a70c86f59a7510c53bbad685a6016befe
David Bitonneau <david.bitonneau@gmail.com>
2015-05-13 19:48:01 +0200

    plot tool: adding selection of motor speed or distance/angle values display

--- tools/pc/plot/anim_plot.py
+++ tools/pc/plot/anim_plot.py
@@ -6,6 +6,9 @@ import matplotlib.animation as animation
 import serial
 from subplot import Subplot, SubplotTable
 
+use_motor_speed_cs = 1
+use_distance_angle_cs = 0
+
 if len(sys.argv) == 1:
     ser = serial.Serial('/dev/ttyUSB0', 115200)
     is_file = False
@@ -17,13 +20,15 @@ fig = plt.figure()
 x_range = 100
 replay_speed = 500 # points replayed per second
 
-#right_motor_subplot = Subplot( -1000, 1000, x_range, fig, 20, 20, 0)
-#left_motor_subplot  = Subplot( -1000, 1000, x_range, fig, 2, 2, 2)
-distance_subplot    = Subplot( -100, 1200, x_range, fig, 1, 1, 1)
-#angle_subplot       = Subplot( -400,  400,  x_range, fig, 2, 2, 4)
-#angle_subplot.hide()
+if (use_motor_speed_cs):
+    right_motor_subplot = Subplot( -100, 2000, x_range, fig, 1, 2, 1)
+    left_motor_subplot  = Subplot( -100, 2000, x_range, fig, 1, 2, 2)
+elif (use_distance_angle_cs):
+    distance_subplot    = Subplot( -400, 2400, x_range, fig, 2, 2, 1)
+    angle_subplot       = Subplot( -30, 450,  x_range, fig, 2, 2, 3)
+    #angle_subplot.hide()
 
-#robot_subplot       = SubplotTable(fig, 20, 20, 0)
+    robot_subplot       = SubplotTable(fig, 1, 2, 2)
 
 def readline_starting_with(start_string):
     c = 'a'
@@ -46,22 +51,25 @@ def readline_starting_with(start_string):
         return readline_starting_with(start_string)
 
 def animate(frame_num):
-    #l = right_motor_subplot.update_lines(frame_num, readline_starting_with("Right"))
-    #l.extend(left_motor_subplot.update_lines(frame_num, readline_starting_with("Left")))
-    l = distance_subplot.update_lines(frame_num, readline_starting_with("Distance"))
-    #l.extend(angle_subplot.update_lines(frame_num, readline_starting_with("Angle")))
-    #l.extend(robot_subplot.update_lines(frame_num, readline_starting_with("Robot")))
+    if (use_motor_speed_cs):
+        l = right_motor_subplot.update_lines(frame_num, readline_starting_with("Right"))
+        l.extend(left_motor_subplot.update_lines(frame_num, readline_starting_with("Left")))
+    elif (use_distance_angle_cs):
+        l = distance_subplot.update_lines(frame_num, readline_starting_with("Distance"))
+        l.extend(angle_subplot.update_lines(frame_num, readline_starting_with("Angle")))
+        l.extend(robot_subplot.update_lines(frame_num, readline_starting_with("Robot")))
 
     return l
 
 #Reset only required for blitting to give a clean slate.
 def reset():
-    #l = right_motor_subplot.reset()
-    #l.extend(left_motor_subplot.reset())
-    #l.extend(distance_subplot.reset())
-    l = distance_subplot.reset()
-    #l.extend(angle_subplot.reset())
-    #l.extend(robot_subplot.reset())
+    if (use_motor_speed_cs):
+        l = right_motor_subplot.reset()
+        l.extend(left_motor_subplot.reset())
+    elif (use_distance_angle_cs):
+        l = distance_subplot.reset()
+        l.extend(angle_subplot.reset())
+        l.extend(robot_subplot.reset())
     return l
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 200000), init_func=reset,
